63316886ec0a123bfab916235fe10ba548b8e2f6
Marcos Martinez Jaime <marccoswq@gmail.com>
2026-09-14 21:03:18 -0300

    Update hashFunctions.h

--- include/hashFunctions.h
+++ include/hashFunctions.h
@@ -3,6 +3,9 @@
 #include <vector> 
 #include <cstdint> 
 
+std::cout << std::boolalpha;
+std::cout << "Check 'usuario1@email.com': " << bloomFilter.checkItem("usuario1@email.com") << "\n";
+std::cout << "Check 'desconocido@email.com': " << bloomFilter.checkItem("desconocido@email.com") << "\n";
 namespace hashFunctions { 
     uint32_t fnv1aHash(const std::string& key); 
     uint32_t djb2Hash(const std::string& key); 
